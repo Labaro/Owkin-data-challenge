366d5cd69a2f540b09b86c8ac7a41737023fac84
Labaro <e.levecque@hotmail.fr>
2020-02-25 17:42:17 +0100

    working coxPH 0.6-0.74

--- crash_test.py
+++ crash_test.py
@@ -1,44 +1,36 @@
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.neighbors import KNeighborsRegressor
-import seaborn as sns
-from utils import *
+import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn import preprocessing
+from sksurv.linear_model import CoxPHSurvivalAnalysis
+from sksurv.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sksurv.util import *
+import numpy as np
 
 
-def plotAutocovariance(data):
-    ACov = np.cov(data, rowvar=False, bias=False)
-    print('Covariance matrix:\n', ACov)
+data_x = pd.read_csv('train/features/radiomics.csv')
+data_y = pd.read_csv('train/output.csv')
+print(data_x.columns)
+X = data_x[['shape.4', 'shape.6', 'shape.2', 'textural.9', 'textural.10', 'textural.11']].values[2:].astype(float)
+y = np.stack([data_y['Event'].astype(bool).values, data_y['SurvivalTime'].astype(int).values], axis=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-    cmap = sns.color_palette("GnBu", 40)
-    sns.heatmap(ACov, cmap=cmap, vmin=0)
-    plt.show()
-    return ACov
+X_train = pd.DataFrame(X_train, columns=['shape.4', 'shape.6', 'shape.2', 'textural.9', 'textural.10', 'textural.11'])
 
+y_train = Surv().from_arrays(y_train[:, 0], y_train[:, 1])
+y_test = Surv().from_arrays(y_test[:, 0], y_test[:, 1])
 
-train_output = pd.read_csv('train/output.csv', index_col=0)
-train_input = pd.read_csv('train/features/radiomics.csv', index_col=0)
-clinical = pd.read_csv('train/features/clinical_data.csv', index_col=0)
+estimator = CoxPHSurvivalAnalysis()
+estimator.fit(X_train, y_train)
 
-scaler = preprocessing.StandardScaler()
-X = scaler.fit_transform(train_input.values[2:].astype(float))
-y = train_output['SurvivalTime'].values.astype(float)
-index = train_output.index
-Z = clinical[['Mstage', 'Nstage', 'Tstage']].values.astype(float)
-
-A = np.cov(np.hstack((X, y.reshape((-1, 1)))), rowvar=False)
-b = np.linalg.norm(A, ord=1, axis=0)
-arg = np.argsort(A[-1, :-1])[:10]
-print(train_input.values[0, arg])
-
-
-X_train, X_test, y_train, y_test, idx_train, idx_test = get_random_train_test(np.hstack((X[:, arg], Z)), y, 340, index)
+print(estimator.coef_)
+print(estimator.score(X_test, y_test))
+pred_surv = estimator.predict_survival_function(X_test)
+for i, c in enumerate(pred_surv):
+    plt.step(c.x, c.y, where="post", label="Sample %d" % (i + 1))
+plt.ylabel("est. probability of survival $\hat{S}(t)$")
+plt.xlabel("time $t$")
+plt.legend(loc="best")
+plt.show()
 
 
-rf = RandomForestRegressor(n_estimators=100, criterion='mse', oob_score=True)
-knn = KNeighborsRegressor(n_neighbors=5)
 
-
-print("Random Forest: ", cross_validation(rf, X, y, index, 6))
-print("K-nn: ", cross_validation(knn, X, y, index, 6))
-print("Average of both: ", cross_validation_average([knn, rf], X, y, index, 6))
