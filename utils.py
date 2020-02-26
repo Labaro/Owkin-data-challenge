86851187eed15527c57bd7220884afc7d9752ee2
Labaro <e.levecque@hotmail.fr>
2020-02-26 11:50:58 +0100

    new features for accessing data

--- utils.py
+++ utils.py
@@ -1,62 +1,103 @@
-from metrics_t9gbvr2 import *
-from sklearn.base import clone
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sksurv.util import Surv
 
 
-def scorer(y_truth, y_pred, index):
-    """Return the c-index value of a prediction.
-    y_truth: DataFrame
-    y_pred: array"""
-    data = pd.read_csv("train/output.csv", index_col=0)
-    d_truth = {'SurvivalTime': y_truth.tolist(), 'Event': data['Event']}
-    df_truth = pd.DataFrame(d_truth, index=index)
-    d_pred = {'SurvivalTime': y_pred.tolist(), 'Event': np.zeros(y_pred.shape[0])}
-    df_pred = pd.DataFrame(d_pred, index=index)
-    return cindex(df_truth, df_pred)
+def get_data():
+    """Read radiomics, clinical and output csv files and returns data after being homogenize.
+
+    :return pd.Dataframe X: samples features.
+    :return pd.DataFrame y: output.
+    """
+    radiomics = pd.read_csv('train/features/radiomics.csv')
+    df_radio = pd.DataFrame(radiomics.values[2:, 1:].astype(float),
+                            columns=radiomics.values[0, 1:])
+
+    clinical = pd.read_csv('train/features/clinical_data.csv')
+    clinical_array = clinical.values
+
+    # Homogenization of Histology feature:
+    hist = np.apply_along_axis(lambda x: np.char.lower(x), 0, clinical_array[:, 1].astype(str))
+    hist[np.flatnonzero(np.core.defchararray.find(hist, 'nos') != -1)] = 'nos'  # find every elements containing 'nos'
+    for name in ['adenocarcinoma', 'large cell', 'squamous cell carcinoma', 'nos', 'nan']:
+        clinical[name] = (hist == name).astype(int)
+    del clinical['Histology']
+
+    # Homogenization of SourceDataset feature:
+    source = clinical_array[:, 4]
+    for name in ['l1', 'l2']:
+        clinical[name] = (source == name).astype(int)
+    del clinical['SourceDataset']
+
+    del clinical['PatientID']
+
+    # Concatenation with radiomics file:
+    X = pd.DataFrame(np.hstack([clinical.values, df_radio.values]),
+                     columns=np.concatenate((clinical.columns, df_radio.columns)))
+
+    # Homogenization of output file:
+    y = pd.read_csv('train/output.csv')
+    y['Event'] = y['Event'] == 1
+    del y['PatientID']
+
+    return X, y
 
 
-def get_random_train_test(X, y, n, index):
-    idx = np.random.randint(0, y.size, n)
-    idx_C = np.setdiff1d(np.arange(y.size), idx)
-    X_train, y_train = X[idx], y[idx]
-    X_test, y_test = X[idx_C], y[idx_C]
-    index_train = index[idx]
-    index_test = index[idx_C]
-    return X_train, X_test, y_train, y_test, index_train, index_test
+def separate_data(X, y, separator=None):
+    """Separate the data depending on histology or dataset source.
+    :parameter separator: optional, str or None (default=None).
+                            - 'Histology' > return one DataFrame per different histology
+                            - 'SourceDataset' > return one Dataframe per different dataset source
+    :returns X, y: list of """
+    Xs, ys = [], []
+    if separator is None:
+        return [X], [y]
+    elif separator == 'Histology':
+        names = ['adenocarcinoma', 'large cell', 'squamous cell carcinoma', 'nos', 'nan']
+        for name in names:
+            index = np.flatnonzero(X[name])
+            Xs.append(X.iloc[index].drop(names, axis=1))
+            ys.append(y.iloc[index])
+    elif separator == 'SourceDataset':
+        names = ['l1', 'l2']
+        for name in names:
+            index = np.flatnonzero(X[name])
+            Xs.append(X.iloc[index].drop(names, axis=1))
+            ys.append(y.iloc[index])
+    return Xs, ys
 
 
-def get_train_test(X, y, a, b, index):
-    idx = np.arange(a, b)
-    idx_C = np.setdiff1d(np.arange(y.size), idx)
-    X_train, y_train = X[idx_C], y[idx_C]
-    X_test, y_test = X[idx], y[idx]
-    index_train = index[idx_C]
-    index_test = index[idx]
-    return X_train, X_test, y_train, y_test, index_train, index_test
+def select_features(X, features=None):
+    output = []
+    if features is None:
+        output = X
+    else:
+        for x in X:
+            x.drop(np.setdiff1d(x.columns, features), axis=1)
+            output.append(x)
+    return output
 
 
-def cross_validation(model, X, y, index, n):
-    N = X.shape[0]
-    S = 0
-    for i in range(n):
-        X_train, X_test, y_train, y_test, index_train, index_test = get_train_test(X, y, (N // n) * i, (N//n) * (i + 1),
-                                                                                   index)
-        mdl = clone(model)
-        mdl.fit(X_train, y_train)
-        y_pred = mdl.predict(X_test)
-        S += scorer(y_test, y_pred, index_test)
-    return S/n
+def encode_data(Xs, ys, normalized=False):
+    output_X, output_y = [], []
+    for x in Xs:
+        if normalized:
+            scaler = preprocessing.StandardScaler()
+            output_X.append(scaler.fit_transform(x.values))
+        else:
+            output_X.append(x.values)
+    for y in ys:
+        output_y.append(Surv().from_arrays(y.values[:, 1], y.values[:, 0]))
+    return output_X, output_y
 
 
-def cross_validation_average(models, X, y, index, n):
-    N = X.shape[0]
-    S = 0
-    for i in range(n):
-        X_train, X_test, y_train, y_test, index_train, index_test = get_train_test(X, y, (N // n) * i, (N//n) * (i + 1),
-                                                                                   index)
-        pred = []
-        for model in models:
-            mdl = clone(model)
-            mdl.fit(X_train, y_train)
-            pred.append(mdl.predict(X_test))
-        S += scorer(y_test, np.average(pred, axis=0, weights=[0.2, 0.8]), index_test)
-    return S / n
+def split(features, test_size=None, separator=None, normalized=False):
+    X, y = get_data()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+    Xs_train, ys_train = separate_data(X_train, y_train, separator=separator)
+    Xs_test, ys_test = separate_data(X_test, y_test, separator=separator)
+    Xs_train, ys_train = encode_data(select_features(Xs_train, features=features), ys_train, normalized=normalized)
+    Xs_test, ys_test = encode_data(select_features(Xs_test, features=features), ys_test, normalized=normalized)
+    return Xs_train, Xs_test, ys_train, ys_test
